76c0927743ab2a12c200800c68c843bc18f53ffd
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-12 15:16:49 -0500

    Add createMarker functionality

--- controller.js
+++ controller.js
@@ -14,8 +14,34 @@ var mapctl = angular.module('myApp',[]).controller('mapCtrl', function($scope){
 
 	//createMarker Function
 	var createMarker = function(city, index){
-		console.log(city);
-		console.log(index);
+		// console.log(city);
+		// console.log(index);
+
+		var latLon = city.latLon.split(',');
+		console.log(latLon);
+		console.log(typeof(latLon));
+		var lat = latLon[0];
+		var lon = latLon[1];
+
+		var marker = new google.maps.Marker({
+			map: $scope.map,
+			position: new google.maps.LatLng(lat, lon),
+			title: city.city,
+			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FE7569'
+		});
+
+        markerContentHTML = '<div class="infoWindowContent">';
+	        markerContentHTML += '<div class="total-pop">Total Population: ' + city.yearEstimate + '</div>';
+    	    markerContentHTML += '<div class="pop-dens-last-year">2010 Census: ' + city.lastCensus + '</div>';
+	        markerContentHTML += '<div class="pop-change">Population Change %: ' + city.change + '</div>';
+    	    markerContentHTML += '<div class="pop-dens">Population Density: ' + city.lastPopDensity + '</div>';
+        	markerContentHTML += '<div class="state">State: ' + city.state + '</div>';
+        	markerContentHTML += '<div class="land-area">Land Area: ' + city.landArea + '</div>';
+        	markerContentHTML += '<a href="#" onclick="getDirections('+lat+','+lon+')">Get directions</a>';
+        markerContentHTML += '</div>';
+
+
+
 	}
 
 	$scope.cities = cities;
