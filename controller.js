6c8b9ab8e9860dd1cbb270b6ce576525f248376e
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-12 14:40:45 -0500

    Update to include list view, add create marker function, add map

--- controller.js
+++ controller.js
@@ -1,11 +1,31 @@
 //Angular App Module and controller
-angular.module('myApp',[]).controller('mapCtrl', function($scope){
+var mapctl = angular.module('myApp',[]).controller('mapCtrl', function($scope){
 
-	var mapOption = {
+	var mapOptions = {
 		zoom: 4,
 		//Center of the US
-		center: new google.maps.LatLng(40.0000, -98.0000),
-		mapTypeId: google.maps.mapTypeId.TERRAIN
+		center: new google.maps.LatLng(40.0000, -98.0000)
 	}
 
+	$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	$scope.markers = [];
+
+	var infoWindow = new google.maps.InfoWindow();
+
+	//createMarker Function
+	var createMarker = function(city, index){
+		console.log(city);
+		console.log(index);
+	}
+
+	$scope.cities = cities;
+	for(i = 0; i < cities.length; i++){
+		createMarker(cities[i],i)
+	}
 });
+
+
+
+
+
+
